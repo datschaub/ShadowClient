8e94edc0fd14375a5371cb43bd7529a6804cc6c4
datschaub <linus__k@hotmail.com>
2015-03-25 13:36:29 +0100

    Css fix

--- WebContent/js/helpers/graphicHelper.js
+++ WebContent/js/helpers/graphicHelper.js
@@ -5,7 +5,8 @@ function GraphicHelper(){
 			$('#' + navbarId).addClass('active');
 		},
 		getTemplateForCharacterContainer: function(id){
-			return '<div id="' + id +  '" class="characterContainer"><div><p>Character Name: <span class="characterName"></span></p></div><div><p>Level: <span class="characterLevel"></span></p></div><div><p>Exp: <span class="characterExp"></span></p></div><div><p>Health Points: <span class="characterCurrentHp"></span>/<span class="characterMaxHp"></span></p></div><div><p>Damage: <span class="characterBaseDmgMin"></span>/<span class="characterBaseDmgMax"></span></p></div><div><p>Amount of Gold: <span class="characterGold"></span></p></div></div';
+			return '<div id="' + id +  '" class="characterContainer"><div><p><h4>Character Name: <span class="characterName"></span></p></h4></div><div><p><img src="img/exp_small.png" class="smallIcon"></img> Exp: <span class="characterExp"></span></p></div><div><p><img src="img/helath_small.png" class="smallIcon"> Health Points: <span class="characterCurrentHp"></span>/<span class="characterMaxHp"></span></p></div><div><p><img src="img/damage_small.png" class="smallIcon"> Damage: <span class="characterBaseDmgMin"></span>/<span class="characterBaseDmgMax"></span></p></div><div><p><img src="img/gold_small.png" class="smallIcon"> Amount of Gold: <span class="characterGold"></span></p></div></div';
 		}
 	}
 }
+
